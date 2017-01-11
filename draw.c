c6c9ac644240781041795ac6a22b1f5e1deb263d
Benjamin TOLLET <btollet@e1r12p9.42.fr>
2017-01-11 17:51:41 +0100

    Line

--- draw.c
+++ draw.c
@@ -17,45 +17,14 @@ void	draw_square(void *mlx, void *win, int *pos)
 	int x;
 	int y;
 
-	y = pos[1] - 1;
-	while (y++ < pos[2])
+	x = pos[1] - 1;
+	while (x++ < pos[2])
 	{
-		x = pos[0] - 1;
-		while (x++ < pos[3])
+		y = pos[0] - 1;
+		while (y++ < pos[3])
 			mlx_pixel_put(mlx, win, x, y, 0x00FFFFFF);
 	}
 }
-
-/*void	draw_line(void *mlx, void *win, int *pos)
-{
-	int x;
-	int y;
-	int int_x;
-	int i;
-	int	j;
-	int test;
-
-	if (pos[2] - pos[0] > pos[3] - pos[1])
-		int_x = (pos[2] - pos[0]) / (pos[3] - pos[1]);
-	else
-		int_x = (pos[3] - pos[1]) / (pos[2] - pos[0]);
-	test = (((pos[3] - pos[1]) * 10) / (pos[2] - pos[0])) % 10;
-
-	y = pos[1] - 1;
-	x = pos[0];
-	j = 0;
-	while (y++ < pos[2])
-	{
-		i = 0;
-		while (i++ < int_x + 1)
-			mlx_pixel_put(mlx, win, x + i - 1, y, 0x00FF0000);
-		if (j++ < test - 2 && test != 0)
-			x++;
-		if (j == 11)
-			j = 0;
-		x += int_x;
-	}
-}*/
 
 void draw_line(void *mlx, void *win, int *pos) 
 {
@@ -64,15 +33,44 @@ void draw_line(void *mlx, void *win, int *pos)
 	int x2 = pos[2];
 	int y2 = pos[3];
 
-	int dx  = x2 - x1,
-          dy  = y2 - y1,
-          y = 0;
-
-      for ( int x = x1; x <= x2; x++ )  
-      {
-      	y = y1 + dy * (x - x1) / dx;
-        mlx_pixel_put(mlx, win, x, y, 0x00FF0000);
-
-      }
+	int dx,dy,i,xinc,yinc,cumul,x,y ;
+	x = x1 ;
+	y = y1 ;
+	dx = x2 - x1 ;
+	dy = y2 - y1 ;
+	xinc = ( dx > 0 ) ? 1 : -1 ;
+	yinc = ( dy > 0 ) ? 1 : -1 ;
+	dx = abs(dx) ;
+	dy = abs(dy) ;
+	mlx_pixel_put(mlx, win, x, y, 0x00FF0000);
+	if ( dx > dy )
+	{
+		cumul = dx / 2 ;
+		for ( i = 1 ; i <= dx ; i++ )
+		{
+			x += xinc ;
+			cumul += dy ;
+			if ( cumul >= dx )
+			{
+				cumul -= dx ;
+				y += yinc ;
+			}
+			mlx_pixel_put(mlx, win, x, y, 0x00FF0000);
+		}
+	}
+	else
+	{
+		cumul = dy / 2 ;
+		for ( i = 1 ; i <= dy ; i++ )
+		{
+			y += yinc;
+			cumul += dx ;
+			if ( cumul >= dy )
+			{
+				cumul -= dy;
+				x += xinc;
+			}
+			mlx_pixel_put(mlx, win, x, y, 0x00FF0000);
+		}
+	}
 }
-
